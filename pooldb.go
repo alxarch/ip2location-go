dcf3963cd62b63d43fa506c20fd9d013810b4072
Sigalas Alexandros <alxarch@gmail.com>
2017-04-19 14:11:35 +0300

    Adds PoolDB

---
+++ pooldb.go
@@ -0,0 +1,39 @@
+package ip2location
+
+import (
+	"sync"
+)
+
+// PoolDB allows for db pooling
+type PoolDB struct {
+	pool    *sync.Pool
+	Factory func() (IP2LocationDB, error)
+}
+
+func (p *PoolDB) Query(ip string, r *Record, m QueryMode) error {
+	if p.pool == nil {
+		p.pool = &sync.Pool{
+			New: func() interface{} {
+				if db, err := p.Factory(); err == nil {
+					return db
+				} else {
+					return errorDB{err}
+				}
+			},
+		}
+	}
+	db := p.pool.Get().(IP2LocationDB)
+	return db.Query(ip, r, m)
+}
+func (p *PoolDB) Close() {
+
+}
+
+type errorDB struct {
+	Error error
+}
+
+func (e *errorDB) Query(string, *Record, QueryMode) error {
+	return e.Error
+}
+func (e errorDB) Close() {}
